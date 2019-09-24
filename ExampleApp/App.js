5acd212bb6b59f7046974629645cb61e351e4f17
Dhia Kennouche <kendhia@gmail.com>
2019-09-24 12:44:23 +0300

    fixed the import library issues -check your notes on howto-

---
+++ ExampleApp/App.js
@@ -0,0 +1,49 @@
+
+import React, {Component} from 'react';
+
+import RNBackgroundJobs from 'react-native-background-jobs'
+
+import {
+  View,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+
+
+export default class App extends Component {
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <View
+        
+        style={styles.container}>
+        <Text>Teeeeest</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+  body: {
+    height: '40%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: '15%',
+  },
+
+});
